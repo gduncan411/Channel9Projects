7720c191144a219dd90e731411ef6c86bfacdb5f
gduncan411@hotmail.com <AtyZ39dXvQJU>
2014-12-18 09:12:54 -0800

    Initial Commit

---
+++ LastWeekOnChannel9/LastWeekOnChannel9UI/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using LastWeekOnChannel9UI.ViewModel;
+
+namespace LastWeekOnChannel9UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+    }
+}
